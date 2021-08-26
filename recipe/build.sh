d0735fe4a4e35a018ef75fdd8726ba5944d5afb9
Kai Tietz <ktietz@anaconda.com>
2021-08-26 19:18:26 +0200

    add support for osx-arm64, and fix build issues for OSX SDK >=11

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,93 +1,89 @@
 #!/bin/bash
+set -x
 
-# this script is based off the homebrew package:
-# https://github.com/Homebrew/homebrew-core/blob/master/Formula/mysql.rb
+# Apple names internal headers 'VERSION' ... sick ... and doesn't include it relative to their file ... sick ...
+# so fix this by renaming those files for the sake of OSX 11 SDKs ...
+mv ./VERSION VERSION_MSQL
+mv ./rapid/plugin/group_replication/libmysqlgcs/VERSION ./rapid/plugin/group_replication/libmysqlgcs/VERSION_MSQL
+mv ./libbinlogevents/VERSION ./libbinlogevents/VERSION_MSQL
 
-mkdir -p build
-cd build
+# Make rpcgen use C Pre Processor provided by the Conda ecosystem. The
+# rpcgen binary assumes that the corresponding binary is always 'cpp'.
+_rpcgen_hack_dir=$SRC_DIR/rpcgen_hack
 
-declare -a _cmake_config_extra
-if [[ ${HOST} =~ .*darwin.* ]]; then
-  _cmake_config_extra+=(-DCMAKE_OSX_SYSROOT=${CONDA_BUILD_SYSROOT})
-elif [[ ${HOST} =~ .*linux.* ]]; then
-  # Force -std=gnu++98 to workaround attempt to create a reference to a reference
-  # in rapid/plugin/group_replication/libmysqlgcs/src/bindings/xcom/gcs_xcom_communication_interface.cc (line 202)
-  re='(.*[[:space:]])\-std\=[^[:space:]]*(.*)'
-  if [[ "${CXXFLAGS}" =~ $re ]]; then
-    CXXFLAGS="${BASH_REMATCH[1]}-std=gnu++98${BASH_REMATCH[2]}"
-  fi
-  # https://gcc.gnu.org/bugzilla/show_bug.cgi?id=82956
-  if [[ ${HOST} =~ .*x86_64.* ]]; then
-    re='(.*)-fvisibility-inlines-hidden(.*)'
-    if [[ "${CXXFLAGS}" =~ $re ]]; then
-      CXXFLAGS="${BASH_REMATCH[1]}${BASH_REMATCH[2]}"
-    fi
-  fi
-  export LDFLAGS="$LDFLAGS -Wl,-rpath-link,$PREFIX/lib"
-  _cmake_config_extra+=(-DRPC_INCLUDE_DIR="${BUILD_PREFIX}/${HOST}/sysroot/usr/include")
-  # The ideal situation would be to have libtirpc and rpcsvc-proto as conda packages
-  # Since our rpcgen has an interpreter which is not the host's program interpreter ...
-  # -bash: .//dev/x86_64-conda_cos6-linux-gnu/sysroot/usr/bin/rpcgen:
-  # /lib/ld-linux-x86-64.so.2: bad ELF interpreter: No such file or directory
-  # ... we need this really nasty hack:
-  ln -s $CPP $BUILD_PREFIX/bin/cpp
-  cat <<'EOF' > $BUILD_PREFIX/bin/rpcgen
+if [[ "${target_platform}" == *"linux"* ]]; then
+    ## We don't have a conda package for rpcgen, but it is present in the
+    ## compiler sysroot on Linux. However, the value of PT_INTERP is not
+    ## convenient for executing it. ('lib' instead of 'lib64')
+    _target_sysroot=$($CXX --print-sysroot)
+    _target_rpcgen_bin=${_target_sysroot}/usr/bin/rpcgen
+    _target_interpreter=${_target_sysroot}/$(patchelf --print-interpreter ${_target_rpcgen_bin})
+    _target_libdir=${_target_sysroot}/$(dirname ${_target_interpreter})
+
+    ## Generate a wrapper which will use the interpreter provided in the
+    ## compiler sysroot to exec rpcgen and also provide the appropriate
+    ## /path/to/dir containing the C Pre Processor (cpp) binary.
+    mkdir -p $_rpcgen_hack_dir/bin
+    cat <<EOF > ${_rpcgen_hack_dir}/bin/rpcgen
 #!/bin/bash
-$(patchelf --print-interpreter /proc/self/exe) ${BUILD_PREFIX}/${HOST}/sysroot/usr/bin/rpcgen -Y $BUILD_PREFIX/bin/ "$@"
+${_target_interpreter} --library-path ${_target_libdir} ${_target_rpcgen_bin} -Y ${_rpcgen_hack_dir}/bin \$@
 EOF
-  chmod +x $BUILD_PREFIX/bin/rpcgen
+    ln -s $(readlink -f ${CPP}) ${_rpcgen_hack_dir}/bin/cpp
+    chmod +x ${_rpcgen_hack_dir}/bin/{rpcgen,cpp}
 
+elif [[ "${target_platform}" == *"osx"* ]]; then
+    ## Unlink GNU Compilers, Clang doesn't provide a separate binary
+    ## for pre processing. So we trick rpcgen to use our Clang instead.
+    mkdir -p $_rpcgen_hack_dir/bin
+    cat <<EOF > ${_rpcgen_hack_dir}/bin/rpcgen
+#!/bin/bash
+rpcgen -Y ${_rpcgen_hack_dir}/bin \$@
+EOF
+    ln -s $BUILD_PREFIX/bin/$(basename ${CC}) ${_rpcgen_hack_dir}/bin/cpp
+    chmod +x ${_rpcgen_hack_dir}/bin/{rpcgen,cpp}
 fi
-LDFLAGS="${LDFLAGS} -Wl,-rpath,${PREFIX}/lib"
-CXXFLAGS="${CXXFLAGS} -fno-strict-aliasing"
-CFLAGS="${CFLAGS} -fno-strict-aliasing"
 
-# -DINSTALL_* are relative to -DCMAKE_INSTALL_PREFIX
-mkdir -p ${PREFIX}/mysql
-cmake \
-    -DCMAKE_PREFIX_PATH=${PREFIX} \
-    -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-    -DINSTALL_INCLUDEDIR=include/mysql \
-    -DINSTALL_MANDIR=share/man \
-    -DINSTALL_DOCDIR=share/doc/mysql \
-    -DINSTALL_DOCREADMEDIR=mysql \
-    -DINSTALL_INFODIR=share/info \
-    -DINSTALL_MYSQLSHAREDIR=share/mysql \
-    -DINSTALL_SUPPORTFILESDIR=mysql/support-files \
-    -DINSTALL_SCRIPTDIR=mysql/scripts \
-    -DCMAKE_CXX=${CXX} \
-    -DCMAKE_CC=${CC} \
-    -DCMAKE_AR=${AR} \
-    -DCMAKE_RANLIB=${RANLIB} \
-    -DCMAKE_LINKER=${LD} \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_FIND_FRAMEWORK=LAST \
-    -DCMAKE_VERBOSE_MAKEFILE=OFF \
-    -DWITH_UNIT_TESTS=OFF \
-    -DDEFAULT_CHARSET=utf8 \
-    -DDEFAULT_COLLATION=utf8_general_ci \
-    -DCOMPILATION_COMMENT=conda-forge \
-    -DWITH_SSL=system \
-    -DWITH_EDITLINE=system \
-    -DDOWNLOAD_BOOST=1 -DWITH_BOOST=${SRC_DIR}/boost \
-    ${_cmake_config_extra[@]} \
-    .. 2>&1 | tee cmake.log
+declare -a _xtra_cmake_args
+if [[ $target_platform == osx-64 ]]; then
+    _xtra_cmake_args+=(-DWITH_ROUTER=OFF)
+    export CXXFLAGS="${CXXFLAGS:-} -D_LIBCPP_DISABLE_AVAILABILITY=1"
+fi
 
-make -j${CPU_COUNT} ${VERBOSE_CM} 2>&1 | tee build.log
-make install ${VERBOSE_CM} 2>&1 | tee install.log
+cmake -S$SRC_DIR -Bbuild -GNinja ${CMAKE_ARGS} \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_PREFIX_PATH="${_rpcgen_hack_dir};$PREFIX" \
+  -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+  -DINSTALL_INCLUDEDIR=include/mysql \
+  -DINSTALL_MANDIR=share/man \
+  -DINSTALL_DOCDIR=share/doc/mysql \
+  -DINSTALL_DOCREADMEDIR=mysql \
+  -DINSTALL_INFODIR=share/info \
+  -DINSTALL_MYSQLSHAREDIR=share/mysql \
+  -DINSTALL_SUPPORTFILESDIR=mysql/support-files \
+  -DINSTALL_SCRIPTDIR=mysql/scripts \
+  -DCMAKE_FIND_FRAMEWORK=LAST \
+  -DCMAKE_VERBOSE_MAKEFILE=OFF \
+  -DWITH_UNIT_TESTS=OFF \
+  -DDEFAULT_CHARSET=utf8 \
+  -DDEFAULT_COLLATION=utf8_general_ci \
+  -DCOMPILATION_COMMENT=conda-forge \
+  -DWITH_SSL=system \
+  -DWITH_EDITLINE=system \
+  -DDOWNLOAD_BOOST=1 -DWITH_BOOST=${SRC_DIR}/boost \
+  -DINSTALL_INCLUDEDIR=include/mysql \
+  -DINSTALL_MANDIR=share/man \
+  -DINSTALL_DOCDIR=share/doc/mysql \
+  -DINSTALL_DOCREADMEDIR=mysql \
+  -DINSTALL_MYSQLSHAREDIR=share/mysql \
+  -DINSTALL_SUPPORTFILESDIR=mysql/support-files \
+  "${_xtra_cmake_args[@]}"
 
-# remove this dir so we do not ship it
-cd ${PREFIX}/mysql-test
-mysql_temp_dir=`mktemp -d ${TMPDIR}/tmp/XXXXXXXXXXXX`
-{
-    set -e
-    # the || here is a rough try...except
-    perl mysql-test-run.pl status --vardir=${mysql_temp_dir} || rm -rf ${mysql_temp_dir}
-}
-cd -
-# always delete anything left
-rm -rf ${mysql_temp_dir}
-rm -rf ${PREFIX}/mysql-test
+if [[ $target_platform == osx-arm64 ]]; then
+    # Update the /path/to/xprotocol_plugin to the one built for the build platform
+    sed -i.bak "s,\(--plugin=protoc-gen-yplg=\)[^ ]*,\1$SRC_DIR/build.codegen/runtime_output_directory/xprotocol_plugin,g" build/build.ninja
+fi
 
-# Make a symlink to script to start the server directly.
+cmake --build build --target install
+
 ln -s ${PREFIX}/mysql/support-files/mysql.server ${PREFIX}/bin/mysql.server
+
