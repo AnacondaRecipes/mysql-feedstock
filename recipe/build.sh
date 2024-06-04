729710ebc50a43cfba9a69580028a81258e2976d
Rafael Martins <rmartins@anaconda.com>
2024-06-04 06:31:54 +0200

    Upgrade to 8.4.0

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 set -x
 
-# Apple names internal headers 'VERSION' ... sick ... and doesn't include it relative to their file ... sick ...
-# so fix this by renaming those files for the sake of OSX 11 SDKs ...
-mv ./VERSION VERSION_MSQL
-mv ./rapid/plugin/group_replication/libmysqlgcs/VERSION ./rapid/plugin/group_replication/libmysqlgcs/VERSION_MSQL
-mv ./libbinlogevents/VERSION ./libbinlogevents/VERSION_MSQL
-
 # Make rpcgen use C Pre Processor provided by the Conda ecosystem. The
 # rpcgen binary assumes that the corresponding binary is always 'cpp'.
 _rpcgen_hack_dir=$SRC_DIR/rpcgen_hack
@@ -18,6 +12,13 @@ if [[ "${target_platform}" == *"linux"* ]]; then
     _target_sysroot=$(realpath $($CXX --print-sysroot))
     _target_rpcgen_bin=${_target_sysroot}/usr/bin/rpcgen
     _target_interpreter=$(realpath ${_target_sysroot}/$(patchelf --print-interpreter ${_target_rpcgen_bin}))
+    if [[ "${target_platform}" == "linux-s390x" ]]; then
+        # FIXME! the interpreter symlink found in the s390x sysroot is broken
+        # (literal 'ld64.so*' instead of 'ld64.so.1').
+        # this workaround assumes that there's exactly one ld-*.so library in the
+        # sysroot. if more than one or no library matches the glob, build will fail.
+        _target_interpreter=$(dirname "${_target_interpreter}")/ld-*.so
+    fi
     _target_libdir=${_target_sysroot}/$(dirname ${_target_interpreter} | rev | cut -d '/' -f 1 | rev)
 
     ## Generate a wrapper which will use the interpreter provided in the
@@ -48,10 +49,12 @@ if [[ $target_platform == osx-64 ]]; then
     _xtra_cmake_args+=(-DWITH_ROUTER=OFF)
     export CXXFLAGS="${CXXFLAGS:-} -D_LIBCPP_DISABLE_AVAILABILITY=1"
 fi
-
-export CXXFLAGS="${CXXFLAGS} -std=c++14"
+if [[ $target_platform == linux-aarch64 ]]; then
+    _xtra_cmake_args+=(-DWITH_BUILD_ID=OFF)
+fi
 
 cmake -S$SRC_DIR -Bbuild -GNinja ${CMAKE_ARGS} \
+  -DCMAKE_CXX_STANDARD=20 \
   -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_PREFIX_PATH="${_rpcgen_hack_dir};$PREFIX" \
   -DCMAKE_INSTALL_PREFIX=${PREFIX} \
@@ -62,30 +65,17 @@ cmake -S$SRC_DIR -Bbuild -GNinja ${CMAKE_ARGS} \
   -DINSTALL_INFODIR=share/info \
   -DINSTALL_MYSQLSHAREDIR=share/mysql \
   -DINSTALL_SUPPORTFILESDIR=mysql/support-files \
-  -DINSTALL_SCRIPTDIR=mysql/scripts \
   -DCMAKE_FIND_FRAMEWORK=LAST \
-  -DCMAKE_VERBOSE_MAKEFILE=OFF \
+  -DADD_GDB_INDEX=OFF \
+  -DMYSQL_MAINTAINER_MODE=OFF \
+  -DWITH_SYSTEM_LIBS=ON \
+  -DWITH_AUTHENTICATION_LDAP=OFF \
   -DWITH_UNIT_TESTS=OFF \
   -DDEFAULT_CHARSET=utf8 \
   -DDEFAULT_COLLATION=utf8_general_ci \
-  -DCOMPILATION_COMMENT=conda-forge \
-  -DWITH_SSL=system \
-  -DWITH_EDITLINE=system \
-  -DDOWNLOAD_BOOST=0 -DWITH_BOOST=${SRC_DIR}/boost \
-  -DINSTALL_INCLUDEDIR=include/mysql \
-  -DINSTALL_MANDIR=share/man \
-  -DINSTALL_DOCDIR=share/doc/mysql \
-  -DINSTALL_DOCREADMEDIR=mysql \
-  -DINSTALL_MYSQLSHAREDIR=share/mysql \
-  -DINSTALL_SUPPORTFILESDIR=mysql/support-files \
+  -DCOMPILATION_COMMENT=Anaconda \
   "${_xtra_cmake_args[@]}"
-
-if [[ $target_platform == osx-arm64 ]]; then
-    # Update the /path/to/xprotocol_plugin to the one built for the build platform
-    sed -i.bak "s,\(--plugin=protoc-gen-yplg=\)[^ ]*,\1$SRC_DIR/build.codegen/runtime_output_directory/xprotocol_plugin,g" build/build.ninja
-fi
 
 cmake --build build --target install
 
 ln -s ${PREFIX}/mysql/support-files/mysql.server ${PREFIX}/bin/mysql.server
-
