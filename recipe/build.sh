10cf56114e83175e4639eb880d6263fae12f811b
vkhomits <khomitsvalia@gmail.com>
2026-07-03 15:36:05 +0300

    Update version to 9.7.0

--- recipe/build.sh
+++ recipe/build.sh
@@ -29,8 +29,8 @@ cmake -S$SRC_DIR -Bbuild -GNinja ${CMAKE_ARGS} \
   -DWITH_FIDO=none \
   -DWITH_SASL=none \
   -DPROTOBUF_INCLUDE_DIR=${PREFIX}/include \
-  -DDEFAULT_CHARSET=utf8 \
-  -DDEFAULT_COLLATION=utf8_general_ci \
+  -DDEFAULT_CHARSET=utf8mb4 \
+  -DDEFAULT_COLLATION=utf8mb4_0900_ai_ci \
   -DINSTALL_INCLUDEDIR=include/mysql \
   -DINSTALL_MANDIR=share/man \
   -DINSTALL_DOCDIR=share/doc/mysql \
